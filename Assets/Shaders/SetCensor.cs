380b1205e7c977e72f0123f7e4d9032c4d43dca3
MatthewRobertMason <Git@matthewmason.ca>
2019-10-06 01:24:57 -0300

    Censoring

---
+++ Assets/Shaders/SetCensor.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SetCensor : MonoBehaviour
+{
+    public float CensorPixelLevel = 16.0f;
+    
+    // Update is called once per frame
+    void Update()
+    {
+        this.GetComponent<SpriteRenderer>().material.SetFloat("Pixel", CensorPixelLevel);
+    }
+}
