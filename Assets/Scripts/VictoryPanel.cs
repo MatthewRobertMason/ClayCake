3329906d9f739b49dc04c27fe6fb6c267657cce7
MatthewRobertMason <Git@matthewmason.ca>
2019-10-07 16:15:38 -0300

    Portal and overlay

---
+++ Assets/Scripts/VictoryPanel.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VictoryPanel : MonoBehaviour
+{
+    public GameObject victoryPanel;
+
+    public void SetVictoryPanelActive(bool active)
+    {
+        victoryPanel.SetActive(active);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+    
+}
